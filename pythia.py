d50d50e3bba345fb6e7d54a753d19b9a555937c1
Christopher Villalobos <cvillalobos@ufl.edu>
2019-03-29 08:25:49 -0400

    Running from command line

--- pythia.py
+++ pythia.py
@@ -1,8 +1,14 @@
 import pythia.config
+import pythia.io
+import pythia.runner
+import pythia.template
 import pprint
 import logging
 
 logging.getLogger("pythia_app")
 
 config = pythia.config.load_config("sample.json")
+single_run = config["runs"][0]
+peerless = pythia.io.peer(single_run)
+pythia.runner.run_peerless(single_run, peerless, config)
 pprint.pprint(config)
