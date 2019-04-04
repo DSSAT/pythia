d9a70ab6631114343b2712fe591d8c16cfc37b1b
Christopher Villalobos <cvillalobos@ufl.edu>
2019-04-04 15:44:21 -0400

    Implement command-line switches
    * Removed extra runs for sample.json
    * Command-line switches to run the different aspects of
      pythia

--- pythia.py
+++ pythia.py
@@ -1,5 +1,4 @@
 import logging
-import pprint
 
 import pythia.config
 import pythia.dssat
@@ -9,9 +8,26 @@ import pythia.peerless
 logging.getLogger("pythia_app")
 logging.basicConfig(level=logging.INFO)
 
-config = pythia.config.load_config("sample.json")
-single_run = config["runs"][0]
-peerless = pythia.io.peer(single_run, config.get("sample", None))
-pythia.peerless.execute(single_run, peerless, config)
-pprint.pprint(config)
-pythia.dssat.execute(config)
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="JSON configuration file to run")
+    parser.add_argument("--all", action='store_true')
+    parser.add_argument("--setup", action="store_true")
+    parser.add_argument("--run-dssat", action="store_true")
+    parser.add_argument("--analyze", action="store_true")
+    args = parser.parse_args()
+
+    if not args.config:
+        parser.print_help()
+    else:
+        config = pythia.config.load_config(args.config)
+        for run in config.get("runs", []):
+            if args.all or args.setup:
+                peerless = pythia.io.peer(run, config.get("sample", None))
+                pythia.peerless.execute(run, peerless, config)
+            if args.all or args.run_dssat:
+                pythia.dssat.execute(config)
+            if args.analyze:
+                print("Analysis module is still being hooked up")
