9c8947862eee4fcfb60b067c45f68a8d86f73568
Christopher Villalobos <cvillalobos@ufl.edu>
2019-04-04 14:19:29 -0400

    Rename runner to peerless
    * Using a specific module name makes it easier to extend
      over time.

--- pythia.py
+++ pythia.py
@@ -3,7 +3,7 @@ import pprint
 
 import pythia.config
 import pythia.io
-import pythia.runner
+import pythia.peerless
 import pythia.template
 
 logging.getLogger("pythia_app")
@@ -12,5 +12,5 @@ logging.basicConfig(level=logging.INFO)
 config = pythia.config.load_config("sample.json")
 single_run = config["runs"][0]
 peerless = pythia.io.peer(single_run, config.get("sample", None))
-pythia.runner.run_peerless(single_run, peerless, config)
+pythia.peerless.execute(single_run, peerless, config)
 pprint.pprint(config)
