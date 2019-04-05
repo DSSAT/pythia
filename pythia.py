9356e3e12b599372b355d4f81cc062ab479a0f5a
Christopher Villalobos <cvillalobos@ufl.edu>
2019-04-05 08:19:26 -0400

    Add helpful text to argsparse

--- pythia.py
+++ pythia.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="JSON configuration file to run")
-    parser.add_argument("--all", action='store_true')
-    parser.add_argument("--setup", action="store_true")
-    parser.add_argument("--run-dssat", action="store_true")
-    parser.add_argument("--analyze", action="store_true")
-    parser.add_argument("--clean-work-dir", action="store_true")
+    parser.add_argument("--all", action='store_true', help="Run all the steps in pythia")
+    parser.add_argument("--setup", action="store_true", help="Setup DSSAT run structure and files")
+    parser.add_argument("--run-dssat", action="store_true", help="Run DSSAT over the run structure")
+    parser.add_argument("--analyze", action="store_true", help="Run the analysis for the DSSAT runs")
+    parser.add_argument("--clean-work-dir", action="store_true", help="Clean the work directory prior to run")
     args = parser.parse_args()
 
     if not args.config:
