0ce2d6984e0c054cda2845466b8e7783e4f1f75a
Christopher Villalobos <cvillalobos@ufl.edu>
2019-06-07 15:42:40 -0400

    Change to Pool vs ThreadPool
    * Moved to using a callback for when the ASYNC function is completed.
    * Should move to using logging so we can redirect to a file later on in life.

--- pythia/dssat.py
+++ pythia/dssat.py
@@ -2,35 +2,38 @@ import logging
 import multiprocessing as mp
 import os
 import subprocess
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool
 
 
 def _run_dssat(details, config):
     logging.debug("Current WD: {}".format(os.getcwd()))
     command_string = "cd {} && {} A {}".format(details['dir'], config['dssat']['executable'], details['file'])
-    print(".", end="")
+    #print(".", end="", flush=True)
     dssat = subprocess.Popen(command_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = dssat.communicate()
-    print("+", end="")
+    #print("+", end="", flush=True)
     return details['dir'], details['file'], out, err, dssat.returncode
 
 
 def _generate_run_list(config):
+    runlist = []
     for root, _, files in os.walk(config.get("workDir", "."), topdown=False):
         for name in files:
             if name.upper().endswith("X"):
-                yield {"dir": root, "file": name}
+                runlist.append({"dir": root, "file": name})
+    return runlist
+
+
+def display_async(details):
+    print(details)
 
 
 def execute(config):
     pool_size = config.get("threads", mp.cpu_count())
-    pool = ThreadPool(pool_size)
     results = []
-
-    for details in _generate_run_list(config):
-        results.append(pool.apply_async(_run_dssat, (details, config)))
-    pool.close()
-    pool.join()
-    print()
-    for result in results:
-        print(result.get())
+    l = _generate_run_list(config)
+    with Pool(processes=pool_size) as pool:
+        for details in l: #_generate_run_list(config):
+            r = pool.apply_async(_run_dssat, (details, config), callback=display_async)
+        pool.close()
+        pool.join()
