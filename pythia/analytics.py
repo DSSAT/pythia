22913ddc063a1a60e1b5a15b8f469ae13e1b0682
Christopher Villalobos <cvillalobos@ufl.edu>
2019-08-23 14:11:48 -0400

    Add singleOutput file support
    * Use "singleOutput": true in the JSON file (under analytics_setup) to
      trigger one output file.

--- pythia/analytics.py
+++ pythia/analytics.py
@@ -19,19 +19,36 @@ def extract_ll(path):
 
 
 def collateOutputs(run, config):
-    per_pixel_file_name = "{}_{}.csv".format(config.get("per_pixel_prefix", "pp"), run["name"])
-    collected_first_line = False
+    analytics_config = config.get("analytics_setup", {})
+    single = analytics_config.get("singleOutput", False)
+    if single:
+        per_pixel_file_name = "{}.csv".format(analytics_config.get("per_pixel_prefix", "pp"))
+    else:
+        per_pixel_file_name = "{}_{}.csv".format(analytics_config.get("per_pixel_prefix", "pp"), run["name"])
+    collected_first_line = analytics_config.get("collectedFirstLine", False)
+    print("Value of CFL: {}".format(collected_first_line))
     work_dir = get_run_basedir(run, config)
     for current_dir in _generated_run_files(work_dir, "summary.csv"):
         lat, lng = extract_ll(current_dir)
+        if single:
+            out_dir = config.get("workDir", ".")
+        else:
+            out_dir = work_dir
+        # Remove the original file before appending
+        if collected_first_line:
+            mode = "a"
+        else:
+            mode = "w"
         with open(os.path.join(current_dir, "summary.csv")) as source, open(
-            os.path.join(work_dir, per_pixel_file_name), "a"
+            os.path.join(out_dir, per_pixel_file_name), mode 
         ) as dest:
             for i, line in enumerate(source):
                 if i == 0:
                     if not collected_first_line:
                         dest.write("LATITUDE,LONGITUDE,{}\n".format(line.strip()))
                         collected_first_line = True
+                        if single:
+                            analytics_config["collectedFirstLine"] = True
                 else:
                     dest.write("{},{},{}\n".format(lat, lng, line.strip()))
 
