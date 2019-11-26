d9826bdfd8873b90cd301a0a9c08dd9eff096225
Christopher Villalobos <cvillalobos@ufl.edu>
2019-11-26 15:49:07 -0500

    Conform auto_planting_window to new template
    * `auto_planting_window` function automatically converted to julian date.
      This is now handled by the template function.

--- pythia/functions.py
+++ pythia/functions.py
@@ -33,9 +33,9 @@ def auto_planting_window(k, run, context, _):
     td = datetime.timedelta(days=vals[2])
     last = first + td
     return {
-        "pdate": pythia.util.to_julian_date(first),
-        "pfrst": pythia.util.to_julian_date(first),
-        "plast": pythia.util.to_julian_date(last),
+        "pdate": first,
+        "pfrst": first,
+        "plast": last,
     }
 
 
